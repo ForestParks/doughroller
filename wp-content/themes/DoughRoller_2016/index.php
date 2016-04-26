79d8325cb5b519f801d4f51e252fc667c053efd3
Forest Parks <forest.parks@gmail.com>
2016-04-26 16:34:27 +0200

    putting the Index.php back in the theme

---
+++ wp-content/themes/DoughRoller_2016/index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+	<section class="hero">
+		  <div class="row intro">
+		    <div class="small-centered medium-uncentered medium-6 large-7 columns">
+		      <h1>Truth? What truth?</h1>
+		      <p>Don't you want to take a leap of faith? Or become an old man, filled with regret, waiting to die alone!</p>
+		    </div>
+		    <div class="small-centered medium-uncentered medium-6 large-5 columns">
+		      <div class="tech-img"></div>
+		    </div>
+		  </div>
+	</section>
+
+<div id="page" role="main">
+	<article class="main-content">
+	<?php if ( have_posts() ) : ?>
+
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+		<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; // End have_posts() check. ?>
+
+		<?php /* Display navigation to next/previous pages when applicable */ ?>
+		<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+			<nav id="post-nav">
+				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+			</nav>
+		<?php } ?>
+
+	</article>
+	<?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer();
